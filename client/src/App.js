40c99a8e5c3c05cbd71c080f3cfdb6d055d186ac
Chris Kalama <chriskalama96@gmail.com>
2020-12-30 22:22:05 -0600

    took original HTML with Youtube link and threw it in a component named Test

--- client/src/App.js
+++ client/src/App.js
@@ -6,10 +6,7 @@ function App() {
   return (
     <div className="App">
       <div>
-        <h1>Hello React World, Here is a Button!</h1>
-        <button>
-        <a href="https://www.youtube.com/" target="_blank">Click</a>
-        </button>
+        <Test />
         <MusicPlayer />
       </div>
     </div>
---
+++ client/src/components/Test.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Test() {
+    return (
+        <div>
+            <h1>Hello React World, Here is a Button! This is Also a Test Component!</h1>
+        <button>
+        <a href="https://www.youtube.com/" target="_blank">Click</a>
+        </button>
+        </div>
+    )
+}
+
+export default Test
