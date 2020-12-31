1850490b9d1b7cc5e2de1f4bd302a415089d0c5d
Chris Kalama <chriskalama96@gmail.com>
2020-12-30 22:36:12 -0600

    added children containment to Content.js

--- client/src/components/Content.js
+++ client/src/components/Content.js
@@ -1,1 +1,18 @@
 import React from "react";
+import {css ,jsx} from "@emotion/react"
+
+
+//This component has children, it is a Containment concept (https://reactjs.org/docs/composition-vs-inheritance.html) from React. It allows you to put children before and within the div tags when you are not sure what children you will need ahead of time. This is typically common in a Sidebar Component! 
+const Content = ({ children }) => (
+    <div className="Content"
+    css={css `
+        width:calc(100% - 200px);
+        padding:20px;
+        background:#121212;
+        `}
+    >
+        {children}
+    </div>
+)
+
+export default Content 
