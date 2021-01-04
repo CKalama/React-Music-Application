6741b1bbd54d1552eb9beed61f11b8a014fa2a83
Chris Kalama <chriskalama96@gmail.com>
2021-01-03 22:57:32 -0600

    added Playbar and Topbar css styling

--- client/src/components/Playbar.js
+++ client/src/components/Playbar.js
@@ -1,11 +1,21 @@
 import React from 'react'
+import {css, jsx} from "@emotion/react"
 
-function Playbar() {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+const Playbar = ({children}) => (
+    <div
+    className= {css `
+    position:absolute;
+    bottom:0;
+    left:0;
+    width:100%;
+    height:75px;
+    background: #282828;
+    z-index: 99;
+    padding: 20px;
+    `}>
+        {children}
+
+    </div>
+)
 
 export default Playbar
--- client/src/components/Topbar.js
+++ client/src/components/Topbar.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from 'react';
+import {css, jsx} from "@emotion/react";
 
-function Topbar() {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+const TopBar = ({children}) => (
+    <div
+    className="TopBar"
+    csx={css `
+    position:absolute;
+    top:0;
+    height:50px;
+    background: #070707;
+    left:200px;
+    width: calc(100% - 200px);
+    padding: 20px;
+    `}
+    >
+        {children}
+        
+    </div>
+)
+
 
 export default Topbar
