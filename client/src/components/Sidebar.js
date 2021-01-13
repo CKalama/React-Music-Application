cd1e4fb9b7a17e28b092e8c7288f870b0082216b
Chris Kalama <chriskalama96@gmail.com>
2021-01-12 20:39:08 -0600

    refactoring Sidebar component to now an unordered list for playlist section. Also learned new way of writing CSS with emotion/core, now in pseudocode

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -5,43 +5,44 @@ import {css, jsx} from "@emotion/react"
 //Because we are using Webpack, it does not matter what you name this logo. Could be anything. 
 import logo from "../assets/musicReactLogo.png"
 
-const Sidebar = ({children}) => (
-    <div
-        className="Sidebar"
-        css={css `
-            width: 170px;
-            height: 875px;
-            background: green;
-            padding: 15px;
-            `}
+const Sidebar = () => {
+    return ( 
+    <ul className="Sidebar" css={sidebarCSS}>
         
-    >
-        
-        <img src={logo} 
-        css={css `
-            height:100px;
-            margin-left:10px;
-            
-            `
-            } 
-        />
+        <img src={logo} />
 
-        <button css={buttonCSS}>
+        <button>
         <a href="https://www.youtube.com/" target="_blank">Click</a>
         </button>
-        {children}
 
-    </div>
+    </ul>
 
-)
+    )}
+    
 
-const buttonCSS = css `
-width:100px;
-text-align:center;
-margin-left:25px;
-border-color:red;
-font-family: 'Roboto Slab', serif;
 
+
+    
+    //New Way of writng CSS with Emotion/core, Now I can even use classNames to invoke CSS below and just do all CSS styling below 
+const sidebarCSS = css `
+    width: 170px;
+    height: 850px;
+    background: green;
+    padding: 15px;
+
+    img {
+        height:100px;
+        margin-left:10px;
+    }
+
+    button {
+    width:100px;
+    text-align:center;
+    margin-left:25px;
+    border-color:red;
+    font-family: 'Roboto Slab', serif; 
+    }
 `
+
 
 export default Sidebar
