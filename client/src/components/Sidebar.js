a1af5d60d1b1a778f79f1fd7d64eaf16541ecbe7
Chris Kalama <chriskalama96@gmail.com>
2021-01-12 21:45:20 -0600

    set up setState in Sidebar component, created a playlist list item using JSX and setState, styled li in css below

--- client/src/components/Sidebar.js
+++ client/src/components/Sidebar.js
@@ -1,19 +1,37 @@
 /** @jsxImportSource @emotion/react */
-import React from 'react'
+import React, {useState} from 'react'
 import {css, jsx} from "@emotion/react"
 //import '../App.css'
 //Because we are using Webpack, it does not matter what you name this logo. Could be anything. 
 import logo from "../assets/musicReactLogo.png"
 
 const Sidebar = () => {
-    return ( 
-    <ul className="Sidebar" css={sidebarCSS}>
-        
-        <img src={logo} />
+    //Setting up setState for   
+    const [playlistState, setPlaylist] = useState({
+        currentPlaylist: '', 
+        playlists: {
+            Home: null,
+            Favorites: null, 
+            EasyToLearn: null,
+        }
+    })
+    
+    //keys -Will give us an array of just the keys in the hook above. 
+    const playlist = Object.keys(playlistState.playlists) 
 
-        <button>
+
+    return ( 
+    <ul className="Sidebar" css={sidebarCSS}>   
+    <img src={logo} />
+
+
+        {/* .map will loop through all the playlists in the state. */}
+        {playlist.map(list => <li key={list}>{list}</li>)}
+
+
+        {/* <button>
         <a href="https://www.youtube.com/" target="_blank">Click</a>
-        </button>
+        </button> */}
 
     </ul>
 
@@ -22,7 +40,7 @@ const Sidebar = () => {
 
 
 
-    
+
     //New Way of writng CSS with Emotion/core, Now I can even use classNames to invoke CSS below and just do all CSS styling below 
 const sidebarCSS = css `
     width: 170px;
@@ -33,6 +51,12 @@ const sidebarCSS = css `
     img {
         height:100px;
         margin-left:10px;
+    }
+
+    li {
+        font-family: 'Roboto Slab', serif;
+        margin-left: 25px; 
+        list-style-type: square;
     }
 
     button {
