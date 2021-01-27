b4a80ceb9d9cf76f0c10fd84cc80de72025a3995
Chris Kalama <chriskalama96@gmail.com>
2021-01-27 01:21:23 -0600

    added psuedocode to current animation css in Toast.js. Will need to modify

--- client/src/components/Toast.js
+++ client/src/components/Toast.js
@@ -17,9 +17,40 @@ const Toast = ({toast, close}) => {
     )
 }
 
-
+//You need to go back and Learn about webkit Animations and alter this so that it is your own. In the Meantime, maybe keep it so you know it works. Go back at end of Final Release and perhaps make your own.
 const CSS = css `
+    -webkit-animation: toast 0.5s cubic-bezier(0.25, 0.46, 0.45, 0.94) both;
+  animation: toast 0.5s cubic-bezier(0.25, 0.46, 0.45, 0.94) both;
+  position: absolute;
+  top: 0;
+  left: calc(50% - 175px);
+  background: white;
+  color: black;
+  width: 350px;
+  padding: 25px;
+  text-align: center;
 
+  @-webkit-keyframes toast {
+    0% {
+      -webkit-transform: translateY(0);
+      transform: translateY(0);
+    }
+    100% {
+      -webkit-transform: translateY(75px);
+      transform: translateY(75px);
+    }
+  }
+
+  @keyframes toast {
+    0% {
+      -webkit-transform: translateY(0px);
+      transform: translateY(0px);
+    }
+    100% {
+      -webkit-transform: translateY(75px);
+      transform: translateY(75px);
+    }
+  }
 `
 
 
