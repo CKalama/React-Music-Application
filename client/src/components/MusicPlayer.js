f93045cda4965f6faa293701ad8c4f418ffe5dda
Chris Kalama <chriskalama96@gmail.com>
2021-02-02 17:58:13 -0600

    added a reducer const with props, then threw those props into the const MusicPlayer useReducer function. Pseudocoded what it does. Created a createContext(null) to wrap all of our components into

--- client/src/components/MusicPlayer.js
+++ client/src/components/MusicPlayer.js
@@ -1,14 +1,16 @@
 /** @jsxImportSource @emotion/react */
-import React, {Reducer, useContext} from 'react'
+import React, {createContext, useReducer } from 'react'
 import {css, jsx} from '@emotion/react'
 import Topbar from "./Topbar";
 import Sidebar from "./Sidebar";
 import Content from "./Content";
 import Playbar from "./Playbar";
 
+export const StoreContext = createContext(null)
+
 const DEFAULT_PLAYLIST = "home";
 
-export const initialState = {
+const initialState = {
     currentPlaylist: DEFAULT_PLAYLIST,
     playlists: {
         home: new Set(),
@@ -17,9 +19,17 @@ export const initialState = {
     }
 };
 
+//Adding Reducer, with props state and action. 
+const reducer = (state, action) => {
+    return state;
+}
+
 
 //At some point, you may need to throw all the components into a MusicPlayer folder. So Taking all the components and storing them within the MusicPlayer component folder.
-const MusicPlayer  = () => (
+const MusicPlayer  = () => {
+    //Using an array, it passes a value of whatever the state is. Which is an object. Dispatch allows us to update the state. useReducer can update and manipulate the state.
+    const [state, dispatch] = useReducer()
+    return (
     <div className="MusicPlayer" css={CSS}>
         <Topbar />
         <Sidebar />
@@ -27,7 +37,8 @@ const MusicPlayer  = () => (
         <Playbar />
         
     </div>
-)
+    )
+}
 
 //First way of writing CSS with emotion/core. You can call it under a const variable and call it above in JSX. 
 //Display: flex; makes it so that any containers will sit side by side. 
